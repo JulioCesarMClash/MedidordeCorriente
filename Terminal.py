d0716f35e27e04f0187a1c4792c8aae1e4d009c3
JulioCesarMClash <engienner.finn.jake@gmail.com>
2019-08-20 15:24:19 -0500

    url Terminal

--- Terminal.py
+++ Terminal.py
@@ -56,7 +56,7 @@ def GeneraArchivo(archivo, numfile):
 def EnvioArchivo():
   path = '/home/pi/Desktop/Muestreos'
   #192.169.9.137 ip lab #######192.168.1.68 ip casa 
-  url = 'https://labmovilidad.unam.mx/tesismonitor/api/test/data'
+  url = 'https://www.labmovilidad.unam.mx/tesismonitor/api/test/data'
   headers = {'Authorization' : '(some auth code)', 'Accept' : 'application/json', 'Content-Type' : 'application/json'}
   while True:
     if len(glob.glob("/home/pi/Desktop/Muestreos/*.json")) == 1:
